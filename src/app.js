07af3fa24dcd9990935795b50a36e50ea4b39225
Murray Lucas <muntact@gmail.com>
2015-11-29 17:48:05 +1300

    Finished initial auth flow.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+/* 3rd party libs */
 import app from 'ampersand-app';
 
+/* our components */
 import Router from './router';
+/* Models */
+import Me from './models/me';
+
+/* Global style bind */
 import './styles/main.styl'
 
 //Bind for console logging.
@@ -8,6 +14,7 @@ window.app = app;
 
 app.extend({
   init() {
+    this.me = new Me();
     this.router = new Router();
     this.router.history.start();
   },
--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 /* 3rd party */
+import app from 'ampersand-app';
 import Router from 'ampersand-router';
 import React from 'react';
 import qs from 'qs';
+import xhr from 'xhr';
 
 /* Top level assets */
 import Layout from './Layout';
@@ -37,13 +39,21 @@ export default Router.extend({
     this.renderPage(<ReposPage/>);
   },
 
-  /*client_id	string	Required. The client ID you received from GitHub when you registered.
-  redirect_uri	string	The URL in your app where users will be sent after authorization. See details below about redirect urls.
-  scope	string	A comma separated list of scopes. If not provided, scope defaults to an empty list of scopes for users that don’t have a valid token for the app. For users who do already have a valid token for the app, the user won’t be shown the OAuth authorization page with the list of scopes. Instead, this step of the flow will automatically complete with the same scopes that were used last time the user completed the flow.
-  state	string	An unguessable random string. It is used to protect against cross-site request forgery attacks.*/
+  /*
+  @param client_id	string	Required. The client ID you received from GitHub when you registered.
+  @param redirect_uri	string	The URL in your app where users will be sent after authorization.
+    See details below about redirect urls.
+  @param scope	string	A comma separated list of scopes. If not provided, scope defaults to an
+    empty list of scopes for users that don’t have a valid token for the app. For users who do
+    already have a valid token for the app, the user won’t be shown the OAuth authorization page
+    with the list of scopes. Instead, this step of the flow will automatically complete with the
+    same scopes that were used last time the user completed the flow.
+  @param state	string	An unguessable random string. It is used to protect against cross-site
+    request forgery attacks.
+  */
   login() {
     window.location = "https://github.com/login/oauth/authorize?" + qs.stringify({
-      client_id: "f8dd69187841cdd22a26",
+      client_id: "f2dbdde63618e6199e89",
       redirect_uri: window.location.origin + "/auth/callback",
       scope: 'user, repo',
     });
@@ -51,7 +61,13 @@ export default Router.extend({
 
   authCallback(query) {
     query = qs.parse(query);
-    console.log(query);
+
+    xhr({
+      url: 'https://still-forest-1632.herokuapp.com/authenticate/' + query.code,
+      json: true,
+    }, (err, req, body) => {
+      app.me.token = body.token;
+    });
   },
 
 })
